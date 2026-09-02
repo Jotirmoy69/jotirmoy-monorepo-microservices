390837cf178be530bc0439d8f17b5050c740e9f2
Jotirmoy69 <jotirmoyff1976@gmail.com>
2026-09-03 01:36:51 +0600

    feat(order-service): add order creation endpoint with auth-service integration

--- order-service/src/app/app.controller.ts
+++ order-service/src/app/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Inject, Param } from '@nestjs/common'; 
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
-@Controller()
+@Controller('orders')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('AUTH_SERVICE') private readonly authClient: ClientProxy){}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Get(':userId')
+  async createOrder(@Param("userId") userId:string){
+    const pattern = {cmd: 'validate_user'};
+    const payload = {userId:Number(userId)}
+    const authRes = await firstValueFrom(this.authClient.send(pattern,payload))
+    if(authRes.status === 'success'){
+      return {message: 'Order created successfully',user:authRes.user}
+    }
+    return {message: 'Order creation failed',error:authRes.message}
   }
 }
